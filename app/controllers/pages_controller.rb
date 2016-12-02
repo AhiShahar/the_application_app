929e29d7701758b9710877787bfa972f0a229dc9
AhiShahar <ahishahar1987@gmail.com>
2016-12-02 11:32:43 +1100

    atom beautify

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     @images = []
     User.all.each do |u|
-      if u.image && u.image != "https://robohash.org/my-own-slug.png?size=50x50&set=set1" && u.image != "" && u.image != "https://robohash.org/my-own-slug.png?size=70x70&set=set1"
+      if u.image && u.image != "https://robohash.org/my-own-slug.png?size=50x50&set=set1" && u.image != "" && u.image != "https://robohash.org/my-own-slug.png?size=70x70&set=set1" && u.is_professional
         @images << u.image
       end
     end
